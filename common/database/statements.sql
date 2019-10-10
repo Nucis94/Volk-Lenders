ee75ebae2568d8e68c96bb54e25c89966cf56f44
onuez <94.nuez@gmail.com>
2019-10-10 23:23:20 +0200

    Define database tables

---
+++ common/database/statements.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE IF NOT EXISTS volk_lenders;
+USE volk_lenders;
+
+
+/*
+* Contains all football players registered.
+*/
+CREATE TABLE IF NOT EXISTS `volk_lenders`.`footballer`
+(
+    `id`         INT(11)      NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name`  VARCHAR(255) NOT NULL,
+    `age`        INT(11)      NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+/*
+* Contains all football teams registered.
+*/
+CREATE TABLE IF NOT EXISTS `volk_lenders`.`team`
+(
+    `id`   INT(11)      NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `city` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+/*
+* Stores a relation between football players and the teams where they have been.
+*/
+CREATE TABLE IF NOT EXISTS `volk_lenders`.`team_footballer`
+(
+    `id`            INT(11)  NOT NULL AUTO_INCREMENT,
+    `team_id`       INT(11)  NOT NULL,
+    `footballer_id` INT(11)  NOT NULL,
+    `datetime`      DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`team_id`) REFERENCES team (`id`),
+    FOREIGN KEY (`footballer_id`) REFERENCES footballer (`id`)
+);
+
+/*
+* Stores all matches and football teams that had played on them.
+*/
+CREATE TABLE IF NOT EXISTS `volk_lenders`.`match`
+(
+    `id`              INT(11)  NOT NULL AUTO_INCREMENT,
+    `local_team_id`   INT(11)  NOT NULL,
+    `foreign_team_id` INT(11)  NOT NULL,
+    `datetime`        DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`local_team_id`) REFERENCES team (`id`),
+    FOREIGN KEY (`foreign_team_id`) REFERENCES team (`id`)
+);
+
+/*
+* Stores all scored goals by footballer, team and match.
+*/
+CREATE TABLE IF NOT EXISTS `volk_lenders`.`goal`
+(
+    `id`            INT(11)  NOT NULL AUTO_INCREMENT,
+    `match_id`      INT(11)  NOT NULL,
+    `team_id`       INT(11)  NOT NULL,
+    `footballer_id` INT(11)  NOT NULL,
+    `datetime`      DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`match_id`) REFERENCES `match` (`id`),
+    FOREIGN KEY (`team_id`) REFERENCES team (`id`),
+    FOREIGN KEY (`footballer_id`) REFERENCES footballer (`id`)
+);
